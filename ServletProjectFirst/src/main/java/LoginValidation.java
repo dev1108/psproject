d543c345c8a21f0be4318b775723d51a08f5ed50
sakshi-badola <65701753+sakshi-badola@users.noreply.github.com>
2023-05-27 11:44:38 +0530

    Update LoginValidation.java

--- ServletProjectFirst/src/main/java/LoginValidation.java
+++ ServletProjectFirst/src/main/java/LoginValidation.java
@@ -22,12 +22,18 @@ public class LoginValidation extends HttpServlet {
 			throws ServletException, IOException {
 
 		// Fetching data from login from
+		
+		
 		String email = request.getParameter("email");
 		String password = request.getParameter("pass");
 
 		// Declared variable for storing data, fetched from database
+		
+		
 		String email1 = null;
 		String password1 = null;
+		boolean admin = false;
+		boolean sadmin = false;
 		try {
 			String url = "jdbc:postgresql://localhost:5432/SignInSignUp";
 			String user = "postgres";
@@ -36,15 +42,17 @@ public class LoginValidation extends HttpServlet {
 			Connection conn = DriverManager.getConnection(url, user, pass);
 
 			// Fetching data from database
-			PreparedStatement ps = conn.prepareStatement("select email, password from signup where password = ?");
+			PreparedStatement ps = conn.prepareStatement("select email, password,isadmin, issuperadmin from signup where email = ?");
 
-			ps.setString(1, password);
+			ps.setString(1, email);
 
 			ResultSet rs = ps.executeQuery();
 
 			while (rs.next()) {
 				email1 = rs.getString(1);
 				password1 = rs.getString(2);
+				admin = rs.getBoolean(3);
+				sadmin = rs.getBoolean(4);
 			}
 
 		} catch (Exception e) {
@@ -52,10 +60,23 @@ public class LoginValidation extends HttpServlet {
 		}
 
 		// Checking data is valid or not && redirecting to user panel area
-		if (email.equals(email1) && password.equals(password1)) {
+		
+		
+		if (email.equals(email1) && password.equals(password1)&& admin==false && sadmin==false) {
 			RequestDispatcher rd1 = request.getRequestDispatcher("LoginUserDashboard");
 			rd1.forward(request, response);
-		} else {
+		} 
+		
+		else if (email.equals(email1) && password.equals(password1) && admin == true && sadmin==false){
+			RequestDispatcher rd3 = request.getRequestDispatcher("LoginAdminDashboard");
+			rd3.forward(request, response);
+		}
+		else if (email.equals(email1) && password.equals(password1) &&admin==false&& sadmin==true) {
+			RequestDispatcher rd1 = request.getRequestDispatcher("super_admin");
+			rd1.forward(request, response);
+		} 
+		
+		else {
 
 			RequestDispatcher rd = request.getRequestDispatcher("LoginError");
 			rd.forward(request, response);
